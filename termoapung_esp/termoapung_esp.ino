6e36fbf3c5b82277ef6b4467dd5672a20beee1ec
Rafi Syah Akram <66723650+syahakram@users.noreply.github.com>
2023-11-05 16:51:23 +0800

    add download feature, add max and min temperature on LCD 16x2

--- termoapung_esp/termoapung_esp.ino
+++ termoapung_esp/termoapung_esp.ino
@@ -4,8 +4,14 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <SD.h>
+#include "RTClib.h"
+#define CSV_PARSER_DONT_IMPORT_SD
+#include <CSV_Parser.h>
 
-String serverName = "http://192.168.1.35:5000/adddata";
+String serverName = "http://192.168.1.249:5000/adddata";
+RTC_DS3231 rtc;
 
 const char* ssid = "BMKG HOME";
 const char* password = "bmkg97012";
@@ -16,51 +22,189 @@ WiFiClient client;
 const int oneWireBus = 2;
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
-LiquidCrystal_I2C lcd(0x27,16,2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+const int chipSelect = 15;
 
 void setup() {
   lcd.begin();
   Serial.begin(9600);
   sensors.begin();
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print('.');
   }
+
+  //Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    return;
+  }
+  Serial.println("card initialized.");
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+  // Adjust waktu
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 }
 
 void loop() {
+  float dtmax = -127;
+  float dtmin = 100;
+  
+  DateTime now = rtc.now();
+  //SD
+  String namefile = "";
+  int sd = now.day();
+  int sm = now.month();
+  if (sd < 10) {
+    namefile += String(0) + String(now.day());
+  } else {
+    namefile += String(now.day());
+  }
+  if (sm < 10) {
+    namefile += String(0) + String(now.month());
+  } else {
+    namefile += String(now.month());
+  }
+  namefile += String(now.year());
+  namefile += ".csv";
+  
+  File dataFile = SD.open(namefile);
+  // Mengirimkan data ke SD
+  if (SD.exists(namefile)) {
+    Serial.println("log.csv exists");
+  } else {
+    dataFile = SD.open(namefile, FILE_WRITE);
+    Serial.println("create log.csv");
+    dataFile.println("dt,tavg,tmax,tmin");
+    dataFile.close();
+  }
+  dataFile = SD.open(namefile);
+  if (dataFile) {
+    CSV_Parser cp(/*format*/ "sfff", /*has_header*/ true, /*delimiter*/ ',', /*quote_char*/ "'");
+    while (dataFile.available()) {
+      cp << (char)dataFile.read();
+    }
+    dataFile.close();
+    Serial.println("Mengambil data dari SD Card...");
+
+    Serial.println("Mengolah data...");
+    char** datetime = (char**)cp["dt"];
+    float* atavg = (float*)cp["tavg"];
+    float* atmax = (float*)cp["tmax"];
+    float* atmin = (float*)cp["tmin"];
+
+    int tmaxlen = cp.getRowsCount();
+    int tminlen = cp.getRowsCount();
+
+    for (int i = 0; i < tmaxlen; i++) {
+      if (dtmax < atmax[i]) {
+        dtmax = atmax[i];
+      }
+    }
+    for (int i = 0; i < tminlen; i++) {
+      if (dtmin > atmin[i]) {
+        dtmin = atmin[i];
+      }
+    }
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Suhu Max:");
+    lcd.setCursor(0, 1);
+    lcd.print(dtmax);
+    delay(1000);
+    lcd.clear();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Suhu Min:");
+    lcd.setCursor(0, 1);
+    lcd.print(dtmin);
+    delay(1000);
+    lcd.clear();
+    
+    Serial.println();
+    Serial.print('Tmax: ');
+    Serial.print(dtmax);
+    Serial.print(', Tmin: ');
+    Serial.println(dtmin);
+    Serial.println();
+  } else {
+    Serial.println("error opening log.csv");
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Error opening");
+    lcd.setCursor(0, 1);
+    lcd.print(namefile);
+    delay(500);
+    lcd.clear();
+  }
+  dataFile = SD.open(namefile, FILE_WRITE);
+
   float tmin = 1000;
   float tmax = 0;
   float tavg = 0;
   float t[60];
+  int jml = 0;
 
   for (int i = 0; i < 60; i++) {
     sensors.requestTemperatures();
     float temperatureC = sensors.getTempCByIndex(0);
-    t[i] = temperatureC;
+    if (t[i] != -127) {
+      t[i] = temperatureC;
+    } else {
+      t[i] = -127;
+    }
     Serial.print(temperatureC);
     Serial.println("ºC");
     lcd.backlight();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("Suhu:");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(t[i]);
+    delay(1000);
+    lcd.clear();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Suhu Max:");
+    lcd.setCursor(0, 1);
+    lcd.print(dtmax);
+    delay(1000);
+    lcd.clear();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Suhu Min:");
+    lcd.setCursor(0, 1);
+    lcd.print(dtmin);
     delay(1000);
     lcd.clear();
   }
 
   for (int i = 0; i < 60; i++) {
-    if (t[i] > tmax) {
-      tmax = t[i];
+    if (t[i] > -127) {
+      if (t[i] > tmax) {
+        tmax = t[i];
+      }
+      if (t[i] < tmin) {
+        tmin = t[i];
+      }
+      tavg = tavg + t[i];
+      jml++;
     }
-    if (t[i] < tmin) {
-      tmin = t[i];
-    }
-    tavg = tavg + t[i];
   }
 
-  tavg = tavg / 60;
+  tavg = tavg / jml;
 
   // Localhost
   if (WiFi.status() == WL_CONNECTED) {
@@ -81,21 +225,74 @@ void loop() {
       Serial.println(httpResponseCode);
       String payload = http.getString();
       Serial.println(payload);
-    }
-    else {
+    } else {
       Serial.print("Error code: ");
       Serial.println(httpResponseCode);
     }
     // Free resources
     http.end();
     lcd.backlight();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("Data sent");
     lcd.clear();
   } else {
     lcd.backlight();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("Data not send");
+    delay(500);
+    lcd.clear();
+  }
+
+  String dataString = "'";
+  if (sd < 10) {
+    dataString += String(0) + String(now.day());
+  } else {
+    dataString += String(now.day());
+  }
+  if (sm < 10) {
+    dataString += String(0) + String(now.month());
+  } else {
+    dataString += String(now.month());
+  }
+  dataString += String(now.year());
+  dataString += "T";
+  if (now.hour() < 10) {
+    dataString += String(0) + String(now.hour());
+  } else {
+    dataString += String(now.hour());
+  }
+  if (now.minute() < 10) {
+    dataString += String(0) + String(now.minute());
+  } else {
+    dataString += String(now.minute());
+  }
+  if (now.second() < 10) {
+    dataString += String(0) + String(now.second());
+  } else {
+    dataString += String(now.second());
+  }
+  dataString += "Z',";
+
+  dataString += String(tavg);
+  dataString += ",";
+  dataString += String(tmax);
+  dataString += ",";
+  dataString += String(tmin);
+
+  dataFile = SD.open(namefile, FILE_WRITE);
+  // Mengirimkan data ke SD
+  if (dataFile) {
+    dataFile.println(dataString);
+    Serial.println(dataString);
+    dataFile.close();
+  } else {
+    Serial.println("error opening log.csv");
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Error opening");
+    lcd.setCursor(0, 1);
+    lcd.print(namefile);
+    delay(500);
     lcd.clear();
   }
   // Thingspeak
--- website/server.py
+++ website/server.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, send_file, jsonify, request
 from waitress import serve
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import json
+import pandas as pd
+import numpy as np
 
 filename = 'D:/termometer-air/static/json/data.json'
 listObj = []
@@ -10,9 +12,13 @@ listObj = []
 app = Flask(__name__, static_url_path="", static_folder='static')
 JSON_FOLDER = 'D:/termometer-air/static/json'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def viewdata():
     return render_template('index.html')
+
+@app.route('/download', methods=['GET'])
+def viewdownload():
+    return render_template('download.html')
 
 @app.route('/returnjson', methods = ['GET'])
 def ReturnJSON():
@@ -20,7 +26,7 @@ def ReturnJSON():
         data = json.load(open('D:/termometer-air/static/json/data.json'))
         return jsonify(data)
     
-@app.route('/adddata', methods=['GET', 'POST'])
+@app.route('/adddata', methods=['GET'])
 def AddData():
     if(request.method == 'GET'):
         with open(filename) as fp:
@@ -130,6 +136,161 @@ def clearData():
 
         return jsonify(data)
 
+@app.route('/returnjsonbydate', methods=['GET'])
+def returnJSONByDate():
+    if(request.method == 'GET'):
+        startdate = str(request.args.get('sd'))
+        enddate = str(request.args.get('ed'))
+        
+        files = [f for f in os.listdir(JSON_FOLDER)]
+        alljson = json.loads('{"feeds": []}')
+
+        for i in range(0, len(files)):
+            thisfile = files[i]
+            jsonpath = os.path.join(JSON_FOLDER, thisfile)
+            with open(jsonpath) as fp:
+                newlistObj = json.load(fp)
+            
+            for i in range(0, len(newlistObj['feeds'])):
+                dt = datetime.strptime(newlistObj['feeds'][i]['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+
+                fromdate = datetime.strptime(startdate, '%Y-%m-%d')
+                todate   = datetime.strptime(enddate, '%Y-%m-%d')
+
+                if dt >= fromdate and dt <= todate+timedelta(days=1):
+                    alljson['feeds'].append({
+                        "created_at": newlistObj['feeds'][i]['created_at'],
+                        "entry_id": newlistObj['feeds'][i]['entry_id'],
+                        "field1": newlistObj['feeds'][i]['field1'],
+                        "field2": newlistObj['feeds'][i]['field2'],
+                        "field3": newlistObj['feeds'][i]['field3']
+                    })
+        
+        return jsonify(alljson)
+
+@app.route('/downloadjsonbydate', methods=['GET'])
+def downloadJSONByDate():
+    if(request.method == 'GET'):
+        startdate = str(request.args.get('sd'))
+        enddate = str(request.args.get('ed'))
+        
+        files = [f for f in os.listdir(JSON_FOLDER)]
+        alljson = json.loads('{"feeds": []}')
+
+        for i in range(0, len(files)):
+            thisfile = files[i]
+            jsonpath = os.path.join(JSON_FOLDER, thisfile)
+            with open(jsonpath) as fp:
+                newlistObj = json.load(fp)
+            
+            for i in range(0, len(newlistObj['feeds'])):
+                dt = datetime.strptime(newlistObj['feeds'][i]['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+
+                fromdate = datetime.strptime(startdate, '%Y-%m-%d')
+                todate   = datetime.strptime(enddate, '%Y-%m-%d')
+
+                if dt >= fromdate and dt <= todate+timedelta(days=1):
+                    alljson['feeds'].append({
+                        "created_at": newlistObj['feeds'][i]['created_at'],
+                        "entry_id": newlistObj['feeds'][i]['entry_id'],
+                        "field1": newlistObj['feeds'][i]['field1'],
+                        "field2": newlistObj['feeds'][i]['field2'],
+                        "field3": newlistObj['feeds'][i]['field3']
+                    })
+        filename = 'D:/termometer-air/static/download/download.json'
+        with open(filename, 'w') as json_file:
+            json.dump(alljson, json_file, indent=4, separators=(',',': '))
+        return send_file(filename, as_attachment=True)
+
+@app.route('/downloaddaily', methods=['GET'])
+def downloadDaily():
+    alldt  = []
+    allt7  = []
+    allt13 = []
+    allt17 = []
+    alltmax= []
+    alltmin= []
+    
+    if(request.method == 'GET'):
+        startdate = str(request.args.get('sd'))
+        enddate = str(request.args.get('ed'))
+        fromdate = datetime.strptime(startdate, '%Y-%m-%d')
+        todate = datetime.strptime(enddate, '%Y-%m-%d')
+        rangeday = todate-fromdate
+        
+        files = [f for f in os.listdir(JSON_FOLDER)]
+        alljson = json.loads('{"feeds": []}')
+
+        for j in range(0, len(files)):
+            thisfile = files[j]
+            jsonpath = os.path.join(JSON_FOLDER, thisfile)
+            with open(jsonpath) as fp:
+                newlistObj = json.load(fp)
+            
+            for i in range(0, len(newlistObj['feeds'])):
+                dt = datetime.strptime(newlistObj['feeds'][i]['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+
+                fromdate = datetime.strptime(startdate, '%Y-%m-%d')
+                todate   = datetime.strptime(enddate, '%Y-%m-%d')
+
+                if dt >= fromdate and dt <= todate+timedelta(days=1):
+                    alljson['feeds'].append({
+                        "created_at": newlistObj['feeds'][i]['created_at'],
+                        "entry_id": newlistObj['feeds'][i]['entry_id'],
+                        "field1": newlistObj['feeds'][i]['field1'],
+                        "field2": newlistObj['feeds'][i]['field2'],
+                        "field3": newlistObj['feeds'][i]['field3']
+                    })
+        
+        date = datetime.strptime(alljson['feeds'][0]['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+        for i in range(0, len(alljson['feeds'])):
+            dt = datetime.strptime(alljson['feeds'][i]['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+            if dt < date:
+                date = dt
+        
+        for d in range(0, rangeday.days+1):
+            days = date+timedelta(days=d)
+            daysbefore = days-timedelta(days=1)
+            t7 = 0
+            t13 = 0
+            t17 = 0
+            tmax = float(-127)
+            tmin = float(100)
+            dday = ''
+            dmonth = ''
+            dyear = ''
+            for i in range(0, len(alljson['feeds'])):
+                thisdate = datetime.strptime(alljson['feeds'][i]['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+                if thisdate.month == days.month:
+                    if thisdate.day == daysbefore.day:
+                        if thisdate.hour == 23 and thisdate.minute > 5 and thisdate.minute <= 10: t7 = float(alljson['feeds'][i]['field1'])
+                    if thisdate.day == days.day:
+                        if thisdate.hour == 5 and thisdate.minute > 5 and thisdate.minute <= 10: t13 = float(alljson['feeds'][i]['field1'])
+                        if thisdate.hour == 9 and thisdate.minute > 5 and thisdate.minute <= 10: t17 = float(alljson['feeds'][i]['field1'])
+                        if tmax < float(alljson['feeds'][i]['field2']): tmax = float(alljson['feeds'][i]['field2'])
+                        if tmin > float(alljson['feeds'][i]['field3']): tmin = float(alljson['feeds'][i]['field3'])
+                        if thisdate.day < 10:
+                            dday = '0'+str(thisdate.day)
+                        else:
+                            dday = str(thisdate.day)
+                        if thisdate.month < 10:
+                            dmonth = '0'+str(thisdate.month)
+                        else:
+                            dmonth = str(thisdate.month)
+                        dyear = str(thisdate.year)
+                        td = dday+'-'+dmonth+'-'+dyear
+
+            alldt.append(td)
+            allt7.append(t7)
+            allt13.append(t13)
+            allt17.append(t17)
+            alltmax.append(tmax)
+            alltmin.append(tmin)
+
+        data = pd.DataFrame({'Tanggal': alldt, 'Suhu jam 07.10': allt7, 'Suhu jam 13.10': allt13, 'Suhu jam 17.10': allt17, 'Suhu maks': alltmax, 'Suhu min': alltmin})
+        data.to_csv('D:/termometer-air/static/download/daily.csv', sep=';', index=False)
+        return send_file('D:/termometer-air/static/download/daily.csv', as_attachment=True)
+
 if __name__ == '__main__':
     #app.run(debug=True)
-    serve(app, host="192.168.1.69", port=5000)
+    serve(app, host="192.168.1.249", port=5000)
